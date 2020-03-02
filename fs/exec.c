ca52e755ca6615f2bd5741730e1d212d8b7109cd
Ondrej <ocky852@gmail.com>
2020-03-02 19:44:05 +0100

    fix use after free in execve

--- fs/exec.c
+++ fs/exec.c
@@ -1796,6 +1796,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 	/* exec_binprm can release file and it may be freed */
 	is_su = d_is_su(file->f_path.dentry);
 
+	/* exec_binprm can release file and it may be freed */
+	is_su = d_is_su(file->f_path.dentry);
+
 	retval = exec_binprm(bprm);
 	if (retval < 0)
 		goto out;
